e7e0db59e9fbc14b5fea8ac3b2e466633095d34c
roshniLingam <lingam_roshni.ghrcecs@raisoni.net>
2023-11-16 15:47:35 +0530

    Created project

---
+++ src/main/java/com/bloggingapp/bloggingapp/BloggingappApplication.java
@@ -0,0 +1,13 @@
+package com.bloggingapp.bloggingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingappApplication.class, args);
+	}
+
+}
