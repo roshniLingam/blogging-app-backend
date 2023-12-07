ffa5b0f7b2730db5106a8c6c105fa158578e6293
roshniLingam <lingam_roshni.ghrcecs@raisoni.net>
2023-12-07 12:26:33 +0530

    Added GlobalExceptionHandler with RestControllerAdvice

---
+++ src/main/java/com/bloggingapp/bloggingapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.bloggingapp.bloggingapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.bloggingapp.bloggingapp.payload.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
+        ApiResponse apiResponse = new ApiResponse(ex.getMessage(), false);
+        return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+}
